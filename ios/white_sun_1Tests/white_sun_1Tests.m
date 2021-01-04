7d05d07a6e0d4bf900deada056a8502307bc4282
Lorence Lim <lorence@crowdbotics.com>
2021-01-04 10:41:22 +0000

    Initial commit.

---
+++ ios/white_sun_1Tests/white_sun_1Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface white_sun_1 : XCTestCase
+
+@end
+
+@implementation white_sun_1
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
